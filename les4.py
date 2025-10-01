532807051dff7fd6303561e05d501117190db780
GeorgianaHut <hutanu_georgiana@yahoo.com>
2025-10-01 14:53:07 +0200

    Les 4 toegevoegd

---
+++ les4.py
@@ -0,0 +1,4 @@
+# boodschappenlijst met minstens 5 items
+boodschappen = ["melk", "brood", "kaas", "eieren", "boter"]
+print("Het eerste item is:", boodschappen[2])
+print("Het derde item is:", boodschappen[3])
