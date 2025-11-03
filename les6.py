66a4638e72ae713abfab55d2a94bc276770029e6
GeorgianaHut <hutanu_georgiana@yahoo.com>
2025-11-03 18:39:27 +0100

    Update les6.py with improved format_namen method

---
+++ les6.py
@@ -0,0 +1,20 @@
+# # opracht 1
+# def format_namen(namen):
+#     # Zorg dat elk woord met een hoofdletter begint
+#     namen = [naam.capitalize() for naam in namen]
+#
+#     # Als er maar één naam is, geef die gewoon terug
+#     if len(namen) == 1:
+#         return namen[0]
+#
+#     # Als er meer dan één naam is
+#     else:
+#         # Voeg de laatste naam toe met 'en'
+#         return ', '.join(namen[:-1]) + ' en ' + namen[-1]
+#
+#
+# # Voorbeeldgebruik
+# lijst = ["mark", "elwyn", "nova", "frans"]
+# resultaat = format_namen(lijst)
+# print(resultaat)  # Output: Mark, Elwyn, Nova en Frans
+
