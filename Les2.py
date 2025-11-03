011532f7c47e26653e06315994bcb4d36e75a5af
GeorgianaHut <hutanu_georgiana@yahoo.com>
2025-11-03 18:42:20 +0100

    Update project files

--- Les2.py
+++ Les2.py
@@ -1,1 +1,40 @@
 
+print("Welkom! We gaan een restaurant kiezen.")
+
+# Laag 1: Kies keuken
+keuken = input("Welke keuken wil je? (italiaans/aziatisch/grill): ")
+
+# Laag 2: Kies budget
+budget = input("Wat is je budget? (low/mid/high): ")
+
+# Laag 3: Kies sfeer
+sfeer = input("Welke sfeer zoek je? (rustig/snel/gezellig): ")
+
+print("\nAdvies:")
+
+if keuken == "italiaans":
+    if budget == "low":
+        print("Pizza Pronto – goedkoop en snel.")
+    elif budget == "mid":
+        print("Trattoria Roma – goede prijs en gezellig.")
+    else:
+        print("Il Teatro – luxe Italiaans restaurant.")
+
+elif keuken == "aziatisch":
+    if budget == "low":
+        print("Noodle Bar – snelle en goedkope ramen.")
+    elif budget == "mid":
+        print("Sushibar – sushibar, middenklasse.")
+    else:
+        print(" Zen – luxe Japans diner.")
+
+elif keuken == "grill":
+    if budget == "low":
+        print("Grill & Go – burgers en friet.")
+    elif budget == "mid":
+        print("Urban Grill – steakhouse, gezellig.")
+    else:
+        print("Steak & Oak – luxe grillrestaurant.")
+
+else:
+    print("Ongeldige keuze, probeer opnieuw.")
--- les4.py
+++ les4.py
@@ -1,4 +1,19 @@
-# boodschappenlijst met minstens 5 items
-boodschappen = ["melk", "brood", "kaas", "eieren", "boter"]
-print("Het eerste item is:", boodschappen[2])
-print("Het derde item is:", boodschappen[3])
+# # boodschappenlijst met minstens 5 items
+# boodschappen = ["melk", "brood", "kaas", "eieren", "boter"]
+# print("Het eerste item is:", boodschappen[2])
+# print("Het derde item is:", boodschappen[3])
+# Puzzle woorden
+# puzzel = ["Albatros", "poncho", "vlinder", "glitter", ]
+# coordinaten = [(0,0),(2,0),(0,-2),(1,3),(0,3),(2,4),(1,-1)]
+#
+# antwoord = [] # sla hier je antwoord in op.
+#
+# for x, y in coordinaten:
+#     letter = puzzel[x][y]
+#     antwoord.append(letter)
+#
+# print(antwoord)
+studenten = [("Georgiana", 8.8), ("Chayenne", 9.0), ("Tygo", 6.2), ("Martijn", 9.1)]
+
+for naam, cijfer in studenten:
+    print(f"{naam}: {cijfer}")
